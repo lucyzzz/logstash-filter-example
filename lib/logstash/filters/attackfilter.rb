a509b79a5fea0944f7e5db769cd46cf7058728e3
lucyzzz <2038937977@qq.com>
2017-12-22 17:36:15 +0800

    bug

--- lib/logstash/filters/attackfilter.rb
+++ lib/logstash/filters/attackfilter.rb
@@ -37,28 +37,23 @@ class LogStash::Filters::attackfilter < LogStash::Filters::Base
     if @message
       # Replace the event message with our message as configured in the
       # config file.
-      attack_filter(@message)
-      #event.set("message", @message)
+      problem = YAML.load(File.open('/opt/rule.yaml'))
+      problem["data"]["sql"].each_with_index do |sql,i|
+          #  puts "id: #{sql['id']}"
+          #  puts "regex: #{sql['regex']}"
+          #  puts "place: #{sql['place']}"
+          #  puts "typename: #{sql['typename']}"
+            reg3 = Regexp.new "#{sql['regex']}"[0..-1] 
+            if reg3 =~ @messages
+              ccc = "检测出存在 #{sql['typename']}  #{@messages}" 
+              event.set("message", ccc)
+              break
+            end
+      end
     end
-
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
   end # def filter
 end # class LogStash::Filters::attackfilter
 
 
-def attack_filter(messages)
-  problem = YAML.load(File.open('/opt/rule.yaml'))
-  problem["data"]["sql"].each_with_index do |sql,i|
-      #  puts "id: #{sql['id']}"
-      #  puts "regex: #{sql['regex']}"
-      #  puts "place: #{sql['place']}"
-      #  puts "typename: #{sql['typename']}"
-       reg3 = Regexp.new "#{sql['regex']}"[0..-1] 
-       if reg3 =~ messages
-          ccc = "检测出存在 #{sql['typename']}  #{messages}" 
-          event.set("message", ccc)
-          break
-       end
-  end
-end
